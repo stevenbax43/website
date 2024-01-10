50f50fd140f731c5ffa3bbe85d3729c8a2d338bb
Bax <steven.bax@kropman.nl>
2024-01-10 11:00:07 +0100

    new instance GCP

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY_prod')  
 
-ALLOWED_HOSTS = ['34.78.189.79','stevenbaxontwerpt.nl','www.stevenbaxontwerpt.nl'] #['*'] = iedereen toegang
+ALLOWED_HOSTS = ['34.77.63.146','stevenbaxontwerpt.nl','www.stevenbaxontwerpt.nl'] #['*'] = iedereen toegang
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
