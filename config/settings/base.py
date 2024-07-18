d0d5bd34558aa85f1b41ecf81ab7602e27731fb9
Bax <steven.bax@kropman.nl>
2024-07-18 14:56:33 +0200

    base

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,7 @@ load_dotenv(find_dotenv())
 # SECURITY WARNING: don't run with debug turned on in production!
 # Git push altijd in Production == False! (Development/local == True) 
 
-DEBUG =  os.environ.get("DEBUG")
+DEBUG =  os.environ.get("DEBUG", default="False").lower() == "true"
 print(type(DEBUG)) #check if DEBUG is an boolean value!! else it is not working
 
 
