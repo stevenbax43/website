b29edc1b512d2fdb079b7f0e1b5519465c1e1924
Bax <steven.bax@kropman.nl>
2024-09-06 14:36:50 +0200

    accounts username

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,17 @@
 # accounts/forms.py
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserChangeForm
 
 class CustomUserChangeForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
-      
+        widgets = {
+            'username': forms.TextInput(attrs={'readonly': 'readonly'}),
+        }
+        labels = {
+            'username': 'Gebruikersnaam (niet te wijzigen)',      # Verbose name for username
+            'email': 'Email-adres',     # Verbose name for email
+            'first_name': 'Voornaam',   # Verbose name for first name
+            'last_name': 'Achternaam',     # Verbose name for last name
+        }
